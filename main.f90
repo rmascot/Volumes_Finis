6cc30d06c45350eb7f6a048c1001c3e9a44ba260
Rémi Mascot <remi.mascot@gmail.com>
2025-01-10 19:00:30 +0100

    test convergence automatique, erreur pas bonne

--- main.f90
+++ main.f90
@@ -1,127 +1,240 @@
 program euler_rusanov_gravity
 
+    ! Utilisation des modules
     use const
     use visualisation
     use functions
 
     implicit none
-  
+
     ! Variables pour les calculs
-    real(PR) :: dx, dt, t, L, g, tmax, lambda1, lambda2, lambda3, vitesse_u, t_sound, c
-    real(PR), dimension(:), allocatable :: P, max_vp, phi_g
-    !real(PR), dimension(3) :: Flux_gauche, Flux_droite
-    real(PR), dimension(:,:), allocatable :: F, U, U_np1, m_div_F, U_analytical, Us
-    real(PR), dimension(:,:), allocatable :: F_arete
-    real(PR), dimension(:,:), allocatable :: source                ! Terme source (gravité)
+    logical :: test_convergence
+    real(PR) :: dx, L, g, tmax, erreur_inf_P, erreur_inf_u
+    real(PR), dimension(:,:), allocatable :: U, U_ref, U_interp, Us
+    real(PR), dimension(:), allocatable :: phi_g, P
+    integer :: nx, nx_ref, i
+    real(PR) :: temp1, temp2, temp3  ! Variables pour lire les colonnes
 
-    integer :: i, nx, j
-  
-    ! Initialisation des variables
-    call read_parameters("parameters.txt", nx, g, L, tmax)
+    ! Initialisation des tableaux pour stocker les erreurs
+    real(PR), dimension(5) :: erreurs_P, erreurs_u
+    integer, dimension(5) :: nx_values
+    character(len=256) :: filename
 
-    ! allocate(U(nx, 3), source(nx, 3))
-    ! allocate(U_analytical(nx, 3))
-    ! allocate(P(nx), max_vp(nx), F_arete(nx-1, 3), F(nx, 3), U_np1(nx, 3), m_div_F(nx, 3))
-    allocate(U(nx+2, 3), source(nx, 3), phi_g(nx+2))
-    allocate(U_analytical(nx+2, 3))
-    allocate(P(nx+2), max_vp(nx+2), F_arete(nx+1, 3), F(nx+2, 3), U_np1(nx+2, 3), m_div_F(nx, 3))
+    ! === PARAMÈTRES UTILISATEUR ===
+    test_convergence = .TRUE.  ! Mettre à .FALSE. pour une seule simulation
 
-    print*, nx, g, L, tmax
-    dx = L / nx
-    call initialize(U, dx, g, phi_g)
-  
-    ! Initialisation du temps
-    t = 0.0
-    j = 0
-    t_sound = 0._PR
+    if (.not. test_convergence) then
+        ! =============================
+        ! CAS SIMPLE : UNE SEULE SIMULATION
+        ! =============================
 
-    ! Boucle temporelle
-    do while (t < tmax)
-    !do j = 1, 2
+        call read_parameters("parameters.txt", nx, g, L, tmax)
 
-        U(2,2) = U(2,1)*((1e-1)*sin(8*pi*t))
+        dx = L / nx
+        allocate(U(nx+2, 3), phi_g(nx+2), P(nx+2))
 
-        ! Calcul de la pression
-        P(2:nx+1) = (gamma - 1._PR) * (U(2:nx+1,3) - 0.5 * U(2:nx+1, 2)**2 / U(2:nx+1, 1))
-        !P(:) = (gamma - 1._PR) * (U(:,3) - 0.5 * U(:, 2)**2 / U(:, 1))
+        call initialize(U, dx, g, phi_g)
+        call simulate(U, P, nx, g, L, tmax, phi_g)
 
-        ! Mise à jour des ghost cells
-        call set_ghost_cells(U, P, nx)
+        allocate(Us(nx+2, 3))
+        call initialize(Us, dx, g, phi_g)
+        call output_results(U, Us, P, nx, L, "solution_reference.txt")
 
-        ! Calcul des max des VP sur chaque maille
-        do i = 1, nx + 2
-            ! Calcul de la vitesse du son c
-            c = sqrt(gamma * P(i) / U(i, 1))
-            
-            ! Vitesse du fluide
-            vitesse_u = U(i, 2) / U(i, 1)
-            
-            ! Valeurs propres
-            lambda1 = vitesse_u - c
-            lambda2 = vitesse_u
-            lambda3 = vitesse_u + c
-            
-            ! Maximum des valeurs propres
-            max_vp(i) = max(abs(lambda1), abs(lambda2), abs(lambda3))
+        deallocate(U, P, phi_g, Us)
+        print*, "Simulation terminée avec succès."
+
+    else
+        ! =============================
+        ! CAS TEST DE CONVERGENCE
+        ! =============================
+
+        call read_parameters("parameters.txt", nx_ref, g, L, tmax)
+
+        allocate(U_ref(nx_ref, 3))
+
+        ! Ouvrir le fichier
+        open(unit=10, file="solution_reference.txt", status="old", action="read")
+    
+        ! Lire les données
+        do i = 1, nx_ref
+            read(10, *) temp1, temp2, temp3
+            U_ref(i, 1) = temp1                   ! Remplir la colonne 1 de U_ref avec la colonne 2 du fichier
+            U_ref(i, 2) = temp2                   ! Remplir la colonne 2 de U_ref avec la colonne 3 du fichier
+            U_ref(i, 3) = temp3                   ! Remplir la colonne 3 de U_ref avec la colonne 4 du fichier
         end do
 
-        ! Calcul du pas de temps basé sur CFL
-        dt = 0.9 * dx / (2*maxval(max_vp(:)))
+        do i = 1, 5
+            nx = 128*2**(i-1)
+            nx_values(i) = nx
+            dx = L / nx
 
-        if (dt <= 0.0 .or. dt > 1.0) then
-            print *, "Erreur: Pas de temps non valide, dt =", dt
-            stop
-        end if
-    
-        ! Calcul des flux avec le schéma de Rusanov
-        call calcul_flux_maille (U, P, F)
+            print*, "Simulation avec nx =", nx
 
-        call calcul_flux_arete(U, F, F_arete, max_vp)
+            allocate(U(nx+2, 3), phi_g(nx+2), P(nx+2))
 
-        call div_flux(F_arete, m_div_F, dx)
-    
-        ! Calcul du terme source de gravité
-        call compute_source(U, source, g)
+            call initialize(U, dx, g, phi_g)
+            call simulate(U, P, nx, g, L, tmax, phi_g)
 
-        !print*, m_div_F(1024, 2)/m_div_F(1024, 1)
-        print*, (source(1024, 2)*(phi_g(1024+2)-phi_g(1024+1))/dx + source(1024, 2)*(phi_g(1024+1)-phi_g(1024))/dx)
-    
-        ! Mise à jour des variables conservées
+            allocate(U_interp(nx+2, 3))
+            call interpolate_solution(U_ref, U_interp, nx_ref, nx)
+
+            allocate(Us(nx+2, 3))
+            call initialize(Us, dx, g, phi_g)
+            ! Construire le nom du fichier
+            write(filename, '(A,I0,A)') "solution_", nx, ".txt"
+            call output_results(U, Us, P, nx, L, filename)
+            call calculate_infinity_norm(U, U_interp, Us, nx, erreur_inf_P, erreur_inf_u)
+
+            erreurs_P(i) = erreur_inf_P
+            erreurs_u(i) = erreur_inf_u
+
+            print*, "Erreur en norme infinie pour nx =", nx
+            print*, "Erreur P :", erreur_inf_P, ", Erreur u :", erreur_inf_u
+
+            deallocate(U_interp, Us)
+            deallocate(U, phi_g, P)
+        end do
+
+        open(unit=20, file="erreurs.txt", status="unknown", action="write")
+        write(20, '(A)') "nx      Erreur_P      Erreur_u"
+        do i = 1, 5
+            write(20, '(I8, F12.6, F12.6)') nx_values(i), erreurs_P(i), erreurs_u(i)
+        end do
+        close(20)
+
+        print*, "Test de convergence terminé. Résultats enregistrés dans 'erreurs.txt'."
+    end if
+
+contains
+
+    ! =============================================
+    ! Fonction pour calculer l'erreur en norme infinie
+    ! =============================================
+    subroutine calculate_infinity_norm(U, U_interp, Us, nx, erreur_inf_P, erreur_inf_u)
+        real(PR), dimension(:,:), intent(in) :: U, U_interp, Us
+        integer, intent(in) :: nx
+        real(PR), intent(out) :: erreur_inf_P, erreur_inf_u
+        real(PR), dimension(:), allocatable :: erreur_P, erreur_u
+        real(PR) :: delta_P, delta_u
+        integer :: i
+
+        allocate(erreur_P(nx), erreur_u(nx))
+
+        do i = 2, nx+1
+            delta_P = abs((U(i, 3) - 0.5 * U(i, 2)**2 / U(i, 1)) - Us(i, 1)**gamma - U_interp(i-1, 2))
+            delta_u = abs(U(i, 2) / U(i, 1) - U_interp(i-1, 3))
+
+            erreur_P(i-1) = delta_P
+            erreur_u(i-1) = delta_u
+        end do
+
+        erreur_inf_P = maxval(erreur_P)
+        erreur_inf_u = maxval(erreur_u)
+
+        deallocate(erreur_P, erreur_u)
+    end subroutine calculate_infinity_norm
+
+    ! ============================================
+    ! Sous-routine pour interpoler la solution fine
+    ! ============================================
+    subroutine interpolate_solution(U_ref, U_interp, nx_ref, nx)
+        real(PR), dimension(:,:), intent(in) :: U_ref
+        real(PR), dimension(:,:), intent(out) :: U_interp
+        integer, intent(in) :: nx_ref, nx
+        integer :: i, j
+        real(PR) :: ratio
+
+        ratio = real(nx_ref) / real(nx)
+
         do i = 1, nx
-            !U_np1(i+1, :) = U(i+1, :) + dt * m_div_F(i, :) + dt * source(i, :)
-            if (i == 1) then
-                U_np1(i+1, :) = U(i+1, :) + dt * m_div_F(i, :) + dt * 0.5 * (source(i, :)*(phi_g(i+2)-phi_g(i+1))/dx + source(i, :)*(phi_g(i+1)-phi_g(i))/dx)
-            else 
-                U_np1(i+1, :) = U(i+1, :) + dt * m_div_F(i, :) + dt * 0.5 * (source(i, :)*(phi_g(i+2)-phi_g(i+1))/dx + source(i-1, :)*(phi_g(i+1)-phi_g(i))/dx)
-            end if
+            j = min(max(1, int(i * ratio)), nx_ref)
+            U_interp(i, :) = U_ref(j, :)
         end do
-    
-        ! Mettre à jour les variables
-        U = U_np1
+    end subroutine interpolate_solution
+
+    subroutine simulate(U, P, nx, g, L, tmax, phi_g)
+
+        ! Variables d'entrée/sortie
+        real(PR), dimension(:,:), intent(inout) :: U
+        real(PR), dimension(:), intent(out) :: P
+        real(PR), dimension(:), intent(in) :: phi_g
+        integer, intent(in) :: nx
+        real(PR), intent(in) :: g, L, tmax
+
+        ! Variables locales
+        real(PR) :: dx, dt, t, lambda1, lambda2, lambda3, vitesse_u, c
+        real(PR), dimension(:), allocatable :: max_vp
+        real(PR), dimension(:,:), allocatable :: F, U_np1, F_arete, m_div_F, source
+        integer :: i, j
+
+        ! Initialisation
+        dx = L / nx
+        allocate(max_vp(nx+2), F(nx+2, 3), U_np1(nx+2, 3), F_arete(nx+1, 3), m_div_F(nx, 3), source(nx, 3))
+
+        ! Initialisation du temps
+        t = 0.0
+        j = 0
+
+        ! Boucle temporelle
+        do while (t < tmax)
+
+            ! Ajout d'une perturbation sinusoïdale
+            U(2, 2) = U(2, 1) * (0.1_PR * sin(8.0_PR * pi * t))
+
+            ! Calcul de la pression
+            P(2:nx+1) = (gamma - 1._PR) * (U(2:nx+1, 3) - 0.5 * U(2:nx+1, 2)**2 / U(2:nx+1, 1))
+
+            ! Mise à jour des ghost cells
+            call set_ghost_cells(U, P, nx)
+
+            ! Calcul des valeurs propres
+            do i = 1, nx + 2
+                c = sqrt(gamma * P(i) / U(i, 1))
+                vitesse_u = U(i, 2) / U(i, 1)
+                lambda1 = vitesse_u - c
+                lambda2 = vitesse_u
+                lambda3 = vitesse_u + c
+                max_vp(i) = max(abs(lambda1), abs(lambda2), abs(lambda3))
+            end do
+
+            ! Calcul du pas de temps basé sur le critère CFL
+            dt = 0.9 * dx / (2 * maxval(max_vp(:)))
+            if (dt <= 0.0 .or. dt > 1.0) then
+                print *, "Erreur: Pas de temps non valide, dt =", dt
+                stop
+            end if
+
+            ! Calcul des flux avec le schéma de Rusanov
+            call calcul_flux_maille(U, P, F)
+            call calcul_flux_arete(U, F, F_arete, max_vp)
+            call div_flux(F_arete, m_div_F, dx)
+
+            ! Calcul du terme source (gravité)
+            call compute_source(U, source, g)
+
+            ! Mise à jour des variables conservées
+            do i = 1, nx
+                if (i == 1) then
+                    U_np1(i+1, :) = U(i+1, :) + dt * m_div_F(i, :) + dt * 0.5 * (source(i, :) * (phi_g(i+2) - phi_g(i+1)) / dx + source(i, :) * (phi_g(i+1) - phi_g(i)) / dx)
+                else
+                    U_np1(i+1, :) = U(i+1, :) + dt * m_div_F(i, :) + dt * 0.5 * (source(i, :) * (phi_g(i+2) - phi_g(i+1)) / dx + source(i-1, :) * (phi_g(i+1) - phi_g(i)) / dx)
+                end if
+            end do
+
+            ! Mise à jour des variables
+            U = U_np1
+            t = t + dt
+            j = j + 1
+        end do
+
+        ! Affichage du nombre d'itérations en temps
+        print*, "Nombre d'itérations en temps : ", j
+
+        ! Libération de la mémoire
+        deallocate(max_vp, F, U_np1, F_arete, m_div_F, source)
+
+    end subroutine simulate
   
-        ! Incrémenter le temps
-        t = t + dt
-
-        j = j + 1
-
-        !print*, "t = ", t
-
-    end do
-
-    print*, "Nombre d'itérations en temps : ", j
-
-    ! Calcul de la solution analytique et de l'erreur
-    allocate(Us(nx+2, 3))
-
-    call initialize(Us, dx, g, phi_g)
-    call compute_analytical(U_analytical, t, nx, dx, L)
-  
-    ! Sauvegarde des résultats dans un fichier
-    call output_results(U, Us, P, nx, L)
-
-    deallocate(U, Us, U_analytical, source)
-    deallocate(P, max_vp, F, U_np1, m_div_F, F_arete)
-  
-  end program euler_rusanov_gravity
+end program euler_rusanov_gravity
   
   
--- visualisation.f90
+++ visualisation.f90
@@ -7,53 +7,59 @@ module visualisation
     contains
 
     !################# Écriture des résultats #################
-    subroutine output_results(U, Us, P, nx, L)
+    ! subroutine output_results(U, Us, P, nx, L)
+    !     real(PR), dimension(:,:), intent(in) :: U, Us
+    !     real(PR), dimension(:), intent(in) :: P
+    !     real(PR), intent(in) :: L
+    !     integer, intent(in) :: nx
+    !     integer :: i
+    !     real(PR) :: x
+    !     character(len=20) :: filename
+    
+    !     filename = "results.txt"
+    !     open(unit=10, file=filename, status="replace")
+    
+    !     write(10,*) "x, rho, rho*u, E, delta_P, delta_u, u_init, P_init"
+    !     do i = 2, nx+1
+    !         x = (i - 1 - 0.5) * L / nx
+    !         write(10,*) x, U(i, 1), U(i, 2), U(i, 3), P(i)-Us(i,1)**gamma, (U(i, 2)/U(i, 1)), (Us(i, 2)/Us(i, 1)), Us(i,1)**gamma
+    !         !write(10,*) x, Us(i, 1), Us(i, 2)/Us(i, 1), U(i, 3), Us(i,1)**gamma, (Us(i, 2)/Us(i, 1))
+    !     end do
+    
+    !     close(10)
+    !     print *, "Résultats enregistrés dans : ", filename
+    ! end subroutine output_results
+
+    subroutine output_results(U, Us, P, nx, L, filename)
+        ! Arguments d'entrée
         real(PR), dimension(:,:), intent(in) :: U, Us
         real(PR), dimension(:), intent(in) :: P
         real(PR), intent(in) :: L
         integer, intent(in) :: nx
+        character(len=*), intent(in) :: filename
+    
+        ! Variables locales
         integer :: i
         real(PR) :: x
-        character(len=20) :: filename
     
-        filename = "results.txt"
+        ! Ouverture du fichier
         open(unit=10, file=filename, status="replace")
     
-        write(10,*) "x, rho, rho*u, E, delta_P, delta_u, u_init, P_init"
+        ! Écriture de l'en-tête
+        !write(10,*) "x, rho, rho*u, E, delta_P, delta_u, u_init, P_init"
+    
+        ! Boucle sur les mailles
         do i = 2, nx+1
             x = (i - 1 - 0.5) * L / nx
-            write(10,*) x, U(i, 1), U(i, 2), U(i, 3), P(i)-Us(i,1)**gamma, (U(i, 2)/U(i, 1)), (Us(i, 2)/Us(i, 1)), Us(i,1)**gamma
-            !write(10,*) x, Us(i, 1), Us(i, 2)/Us(i, 1), U(i, 3), Us(i,1)**gamma, (Us(i, 2)/Us(i, 1))
+            !write(10,*) x, U(i, 1), U(i, 2), U(i, 3), P(i)-Us(i,1)**gamma, (U(i, 2)/U(i, 1)), (Us(i, 2)/Us(i, 1)), Us(i,1)**gamma
+            write(10,*) x, P(i)-Us(i,1)**gamma, (U(i, 2)/U(i, 1))
         end do
     
+        ! Fermeture du fichier
         close(10)
+    
+        ! Message de confirmation
         print *, "Résultats enregistrés dans : ", filename
     end subroutine output_results
-
-    !################# Calcul de la solution exacte #################
-    subroutine compute_analytical(U_analytical, t, nx, dx, L)
-        real(PR), dimension(:,:), intent(out) :: U_analytical
-        real(PR), intent(in) :: t, dx, L
-        integer, intent(in) :: nx
-        integer :: i
-        real(PR) :: x, rho_0, u_0, p_0, epsilon, c_s, k
-    
-        ! Paramètres de l'onde
-        rho_0 = 1.0
-        u_0 = 0.0
-        p_0 = 1.0
-        epsilon = 1.0
-        c_s = sqrt(gamma * p_0 / rho_0)
-        k = 2.0 * pi / L
-    
-        ! Calcul de la solution analytique
-        do i = 1, nx
-            x = (i - 0.5) * dx
-            U_analytical(i, 1) = rho_0 + epsilon * sin(k * x - c_s * t)  ! Densité
-            U_analytical(i, 2) = (rho_0 + epsilon * sin(k * x - c_s * t)) * &
-                                 (u_0 + epsilon * c_s / rho_0 * sin(k * x - c_s * t))  ! Quantité de mouvement
-            U_analytical(i, 3) = p_0 / (gamma - 1) + 0.5 * U_analytical(i, 2)**2 / U_analytical(i, 1)  ! Énergie totale
-        end do
-    end subroutine compute_analytical
 
 end module visualisation
